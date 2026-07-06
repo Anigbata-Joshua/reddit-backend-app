b0eada88b7bf04b4fd6819e4efe26345b14e1207
Josh <joshuaanigbata@gmail.com>
2026-07-06 05:41:51 +0100

    Completed the models. Used the hashed patter for password and also integrated the prefix from the utilis folder

--- models/User.model.js
+++ models/User.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true, trim: true },
     username: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-    password: { type: String, required: true, trim: true },
+    passwordHash: { type: String, required: true, trim: true },
     communities: { type: [String], default: [] },
     karma: { type: Number, default: 0 },
     bio: { type: String, trim: true },
@@ -14,10 +14,19 @@ const userSchema = new mongoose.Schema({
 // Prevent password from been retuned
 userSchema.set('toJSON', {
     transform: (doc, ret) => {
-        delete ret.password;
+        delete ret.passwordHash;
         return ret;
     }
 });
 
 const User = mongoose.model('User', userSchema);
 export default User;
+
+
+// All 5 models are complete:
+
+// User.js ✅
+// Community.js ✅
+// Post.js ✅
+// Comment.js ✅
+// Vote.js ✅
